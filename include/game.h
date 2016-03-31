9f103cda86aabfd519361038b0e870e57cd6794a
n2omatt <n2o.matt@gmail.com>
2016-03-31 05:54:49 -0300

    Add some comments separtors to better structure the code.

--- include/game.h
+++ include/game.h
@@ -8,18 +8,25 @@
 /* Game_Kaboom */
 #include "game_types.h"
 
-/* Screen dimension constants */
+/*******************************************************************************
+* Screen dimension constants                                                   *
+*******************************************************************************/
 extern const int SCREEN_WIDTH;
 extern const int SCREEN_HEIGHT;
 
 
-/* Globals */
+/*******************************************************************************
+* Globals                                                                      *
+*******************************************************************************/
 extern SDL_Window   *g_pWindow;
 extern SDL_Renderer *g_pRenderer;
 extern bool          g_isRunning;
 
 
-/* Public Function Prototypes */
+/*******************************************************************************
+* Public Function Prototypes                                                   *
+*******************************************************************************/
+/* Game loop */
 void game_init(const char *window_name,
                int sdl_window_flags,
                int sdl_renderer_flags);
@@ -27,8 +34,9 @@ void game_init(const char *window_name,
 void game_run(void);
 void game_quit(void);
 
-
+/* Texture Stuff */
 SDL_Texture* game_load_texture(int texture_id);
 void game_unload_texture(int texture_id);
+
 
 #endif /* defined(GAME_H_INCLUDED) */
