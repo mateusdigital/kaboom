1a02fe7812f52428537a578776b2b7ae141fdff5
n2omatt <n2o.matt@gmail.com>
2016-03-31 05:48:20 -0300

    Add the game_textures file This is generate by the script....

---
+++ include/game_textures.h
@@ -0,0 +1,15 @@
+/*
+                  This file is generate by script
+     DO NOT MODIFY IT BY HAND - ALL MODIFICATIONS WILL BE LOST
+    AmazingCow - 2016 - Game_Kaboom - genreate_sprite_headers.py
+*/
+#ifndef __game_textures_h__
+#define __game_textures_h__
+
+
+#define GAME_TEXTURES_IDS_SIZE 1
+extern const char* game_textures_ids[GAME_TEXTURES_IDS_SIZE];
+extern const int game_texture_id_bomb;
+
+
+#endif  /* defined(__game_textures_h__) */
