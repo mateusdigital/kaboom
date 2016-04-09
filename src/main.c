69d3c40e8e9081558026ee8432ef0a1bf5bf6f8d
n2omatt <n2o.matt@gmail.com>
2016-04-08 23:29:48 -0300

    Change the flags.

--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@ int main(int argc, char* args[])
 {
     game_init("Amazing Cow - Kaboom - v0.1",
               SDL_WINDOW_SHOWN,
-              SDL_RENDERER_ACCELERATED);
+              SDL_RENDERER_ACCELERATED |  SDL_RENDERER_PRESENTVSYNC);
 
     game_run();
     game_quit();
