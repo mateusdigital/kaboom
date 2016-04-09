1df70d52fe8ca92e778b2242b9a29caa3c463918
n2omatt <n2o.matt@gmail.com>
2016-04-08 23:31:48 -0300

    - Update the Screen width and height. - Add some cosmetic changes (alignment, ...) - Change the verification to GAME_VERIFY macro - This enables   clean code :D - Add some comments. - Add more verification - Remove the old test stuff.

--- src/game.c
+++ src/game.c
@@ -4,12 +4,11 @@
 #include "helpers.h"
 #include "game_textures.h"
 
-
 /*******************************************************************************
 * Screen dimension constants                                                   *
 *******************************************************************************/
-const int SCREEN_WIDTH  = 640;
-const int SCREEN_HEIGHT = 480;
+const int SCREEN_WIDTH  = 800;
+const int SCREEN_HEIGHT = 600;
 
 
 /*******************************************************************************
@@ -20,15 +19,15 @@ SDL_Window   *g_pWindow   = NULL;
 SDL_Renderer *g_pRenderer = NULL;
 bool          g_isRunning = false;
 /* Private */
-SDL_Texture* g_textures_arr[GAME_TEXTURES_IDS_SIZE] = {NULL};
+SDL_Texture* g_textures_arr[GAME_TEXTURES_IDS_SIZE] = { NULL };
 
 
 /*******************************************************************************
 * Private Function Prototypes                                                  *
 *******************************************************************************/
-void game_update(float dt);
-void game_render(void);
-void game_handle_events(void);
+void game_update       (float dt);
+void game_render       (void    );
+void game_handle_events(void    );
 
 
 /*******************************************************************************
@@ -36,51 +35,71 @@ void game_handle_events(void);
 *******************************************************************************/
 /* Game Loop */
 void game_init(const char *window_name,
-               int sdl_window_flags,
-               int sdl_renderer_flags)
+               int         sdl_window_flags,
+               int         sdl_renderer_flags)
 {
     GAME_LOG("Initializing SDL.");
 
-    if(SDL_Init(SDL_INIT_EVERYTHING) < 0)
-    {
-        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-        abort();
-    }
 
-    g_pWindow = SDL_CreateWindow(window_name,
-                                 SDL_WINDOWPOS_CENTERED,
-                                 SDL_WINDOWPOS_CENTERED,
-                                 SCREEN_WIDTH,
-                                 SCREEN_HEIGHT,
-                                 sdl_window_flags);
+    /* SDL Initialization */
+    GAME_VERIFY(SDL_Init(SDL_INIT_EVERYTHING) >= 0,
+                "SDL could not initialize.");
 
-    g_pRenderer = SDL_CreateRenderer(g_pWindow,
-                                   -1,
-                                   sdl_renderer_flags);
+    /* Window Initialization */
+    g_pWindow = SDL_CreateWindow(window_name,            /* Caption */
+                                 SDL_WINDOWPOS_CENTERED, /* X       */
+                                 SDL_WINDOWPOS_CENTERED, /* Y       */
+                                 SCREEN_WIDTH,           /* Width   */
+                                 SCREEN_HEIGHT,          /* Height  */
+                                 sdl_window_flags);      /* Flags   */
 
+    GAME_VERIFY(g_pWindow, "SDL Cannot create window.");
 
-    game_load_texture(game_texture_id_bomb);
+    /* Renderer Initialization */
+    g_pRenderer = SDL_CreateRenderer(g_pWindow,           /* The window     */
+                                     -1,                  /* First Avaiable */
+                                     sdl_renderer_flags); /* Flags          */
+
+    GAME_VERIFY(g_pRenderer, "SDL Cannot create renderer.");
+
 }
 
 void game_run()
 {
-    GAME_LOG("Running game.");
+    GAME_LOG("Running game...");
 
+    /* Constants */
+    const int   kFPS        = 60;
+    const float kDELAY_TIME = (1000.0f / kFPS);
+
+    /* Framerate vars */
+    Uint32 frame_start;
+    Uint32 frame_time;
+
+    /* Game Loop :D */
     g_isRunning = true;
     while(g_isRunning)
     {
+        frame_start = SDL_GetTicks();
+
         game_handle_events();
-        game_update(0.6);
+        game_update(1.0f / kDELAY_TIME);
         game_render();
 
+        frame_time = SDL_GetTicks() - frame_start;
 
-        SDL_Delay(100);
+        if(frame_time < kDELAY_TIME)
+        {
+            int v = (Uint32)(kDELAY_TIME - frame_time);
+            SDL_Delay(v);
+        }
     }
 }
 
 void game_quit(void)
 {
     GAME_LOG("Quiting game.");
+
     SDL_DestroyRenderer(g_pRenderer);
     SDL_DestroyWindow(g_pWindow);
 
@@ -95,14 +114,22 @@ SDL_Texture* game_load_texture(int texture_id)
     if(g_textures_arr[texture_id])
         return g_textures_arr[texture_id];
 
-    /* Texture isn't loaded yet - Load it and
-       save to the textures array */
+    /* Texture isn't loaded yet - Load it and save to the textures array */
     SDL_Surface *tmp_surface = SDL_LoadBMP(game_textures_ids[texture_id]);
-    SDL_Texture *texture     = SDL_CreateTextureFromSurface(g_pRenderer, tmp_surface);
+    GAME_VERIFY(tmp_surface,
+                "Cannot create surface with filename: %s",
+                game_textures_ids[texture_id]);
 
+    SDL_Texture *texture = SDL_CreateTextureFromSurface(g_pRenderer, tmp_surface);
+    GAME_VERIFY(texture,
+                "Failed to create texture from surface with filename: %s",
+                game_textures_ids[texture_id]);
+
+    /* Prevent leak */
     SDL_FreeSurface(tmp_surface);
 
     g_textures_arr[texture_id] = texture;
+
     return texture;
 }
 void game_unload_texture(int texture_id)
@@ -131,17 +158,25 @@ void game_draw_texture(SDL_Texture *texture,
 *******************************************************************************/
 void game_update(float dt)
 {
+/*
+    Uint8 *k = SDL_GetKeyboardState(0);
+
+    if(k[SDL_SCANCODE_LEFT])
+    if(k[SDL_SCANCODE_RIGHT])
+*/
 }
 void game_render(void)
 {
     SDL_SetRenderDrawColor(g_pRenderer, 255, 0, 255, 255);
     SDL_RenderClear(g_pRenderer);
 
-    game_draw_texture(g_textures_arr[game_texture_id_bomb],
-                      0, 0,
-                      200, 200,
-                      SDL_FLIP_NONE);
 
+    /*
+    game_draw_texture(g_textures_arr[game_texture_id_ballon],
+                      x, 100,
+                      40, 40,
+                      SDL_FLIP_NONE);
+    */
     SDL_RenderPresent(g_pRenderer);
 }
 void game_handle_events(void)
