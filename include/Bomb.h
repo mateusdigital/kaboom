0cd78f91bd896faf8dda79f458a7058c12fe6c38
n2omatt <n2o.matt@gmail.com>
2016-05-21 16:47:55 -0300

    - Add the methods to control the bomb state. - Add the explosion stuff. - Update the CTOR initialization. - Implement the update method. - Implement the draw method. - Refactor some stuff into new methods.

--- include/Bomb.h
+++ include/Bomb.h
@@ -14,6 +14,15 @@ class Bomb : public Lore::IDrawable, public Lore::IUpdatable
 {
     // Enums / Constants / Typdefs //
 public:
+    enum class State
+    {
+        Alive,
+        Exploding,
+        Dead
+    };
+
+    typedef std::function<void ()> ExplodeFinishedCallback;
+    typedef std::function<void ()> ReachTargetCallback;
 
 
     // CTOR / DTOR //
@@ -28,28 +37,53 @@ public:
     virtual void draw()           override;
 
 
+    // Actions //
+public:
+    void reset  ();
+    void explode();
+
+    void startDropping(int turnNumber);
+    void stopDropping ();
+
+
     // Setters //
 public:
-    void setPosition(const Lore::Vector2 &pos)
-    {
-        m_pCurrentSprite->setPosition(pos);
-    }
+    void setPosition(const Lore::Vector2 &pos);
+    void setMovementBounds(const Lore::Vector2 &bounds);
+
+    void setOnReachTargetCallback    (const ReachTargetCallback     &callback);
+    void setOnExplodeFinishedCallback(const ExplodeFinishedCallback &callback);
+
+
+    // Getters //
+public:
+    const Lore::Vector2& getPosition() const;
+    State getState() const;
+
 
     // Private Methods //
 private:
+    //Init
     void initSprites();
+    void initTimers ();
+
+    //Helpers
     void setupFrames(const Lore::Sprite &sprite,
                      std::vector<Lore::Rectangle> &framesVec,
                      int framesCount);
 
     void changeSpriteFrame(int frameIndex);
 
-
     //Timer Callbacks
     void onAnimationTimerTick();
+    void onExplodeTimerTick  ();
+
 
     // iVars //
 private:
+    //HouseKeeping
+    State m_state;
+
     //Sprite / Animation
     Lore::Sprite  m_aliveSprite;
     Lore::Sprite  m_explodedSprite;
@@ -63,8 +97,17 @@ private:
     CoreClock::Clock m_animationTimer;
 
     //Movement / Bounds
+    Lore::Vector2 m_pos;
     Lore::Vector2 m_speed;
     Lore::Vector2 m_initialPosition;
+    Lore::Vector2 m_maxBounds;
+
+    //Exploded
+    CoreClock::Clock m_explodeTimer;
+
+    //Callback
+    ReachTargetCallback     m_reachTargetCallback;
+    ExplodeFinishedCallback m_explodedFinishedCallback;
 };
 
 NS_GAMEKABOOM_END
--- src/Bomb.cpp
+++ src/Bomb.cpp
@@ -12,15 +12,36 @@ constexpr int kSpriteFramesCount_Alive    = 4;
 constexpr int kSpriteFramesCount_Exploded = 3;
 
 constexpr float kTimerSpriteFrameChange = 0.3f;
-
+constexpr float kTimerBombExplode       = 0.3f;
 
 
 ////////////////////////////////////////////////////////////////////////////////
 // CTOR / DTOR                                                                //
 ////////////////////////////////////////////////////////////////////////////////
-Bomb::Bomb()
+Bomb::Bomb() :
+    //HouseKeeping
+    m_state(Bomb::State::Dead),
+    //Sprite / Animation
+    //m_aliveSprite    - Initialized in initSprites()
+    //m_explodedSprite - Initialized in initSprites()
+    m_pCurrentSprite(nullptr),
+    //m_aliveFrames    - Initialized in initSprites()
+    //m_explodedFrames - Initialized in initSprites()
+    m_pCurrentFrames(nullptr),
+    m_frameIndex    (0),
+    //m_animationTimer - Initialized in initTimers()
+    //Movement / Bounds
+    m_pos            (Lore::Vector2::Zero()),
+    m_speed          (Lore::Vector2::Zero()),
+    m_initialPosition(Lore::Vector2::Zero())
+    //Exploded
+    //m_explodeTimer - Initialized in initTimers()
+    //Callback
+    //m_reachTargetCallback      - Default initialized
+    //m_explodedFinishedCallback - Default initialized
 {
     initSprites();
+    initTimers ();
 }
 
 Bomb::~Bomb()
@@ -34,18 +55,115 @@ Bomb::~Bomb()
 ////////////////////////////////////////////////////////////////////////////////
 void Bomb::update(float dt)
 {
+    if(m_state == Bomb::State::Dead)
+        return;
+
     m_animationTimer.update(dt);
+    m_explodeTimer.update  (dt);
+
+    if(m_state == Bomb::State::Alive)
+    {
+        setPosition(getPosition() + (m_speed * dt));
+
+        if(getPosition().y >= m_maxBounds.y)
+            m_reachTargetCallback();
+    }
 }
 
 void Bomb::draw()
 {
+    if(m_state == Bomb::State::Dead)
+        return;
+
+    m_pCurrentSprite->setPosition(m_pos);
     m_pCurrentSprite->draw();
 }
 
 
 ////////////////////////////////////////////////////////////////////////////////
+// Actions                                                                    //
+////////////////////////////////////////////////////////////////////////////////
+void Bomb::reset()
+{
+    m_state = Bomb::State::Dead;
+
+    m_explodeTimer.stop();
+}
+
+void Bomb::explode()
+{
+    m_state = Bomb::State::Exploding;
+
+    m_pCurrentSprite = &m_explodedSprite;
+    m_pCurrentFrames = &m_explodedFrames;
+    changeSpriteFrame(0);
+
+    m_explodeTimer.start();
+}
+
+void Bomb::startDropping(int turnNumber)
+{
+    m_state = Bomb::State::Alive;
+
+    m_pCurrentSprite = &m_aliveSprite;
+    m_pCurrentFrames = &m_aliveFrames;
+    changeSpriteFrame(0);
+
+    m_explodeTimer.stop();
+
+    //COWTODO: Decide the speed;
+    m_speed = Lore::Vector2(0, 200);
+}
+
+void Bomb::stopDropping()
+{
+    m_speed = Lore::Vector2::Zero();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Setters                                                                    //
+////////////////////////////////////////////////////////////////////////////////
+void Bomb::setPosition(const Lore::Vector2 &pos)
+{
+    m_pos = pos;
+}
+
+void Bomb::setMovementBounds(const Lore::Vector2 &bounds)
+{
+    m_maxBounds = bounds;
+}
+
+void Bomb::setOnReachTargetCallback(const ReachTargetCallback &callback)
+{
+    m_reachTargetCallback = callback;
+}
+
+void Bomb::setOnExplodeFinishedCallback(const ExplodeFinishedCallback &callback)
+{
+    m_explodedFinishedCallback = callback;
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Getters                                                                    //
+////////////////////////////////////////////////////////////////////////////////
+const Lore::Vector2& Bomb::getPosition() const
+{
+    return m_pos;
+}
+
+Bomb::State Bomb::getState() const
+{
+    return m_state;
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////
 // Private Methods                                                            //
 ////////////////////////////////////////////////////////////////////////////////
+//Inits
 void Bomb::initSprites()
 {
     //Load the sprites.
@@ -61,19 +179,33 @@ void Bomb::initSprites()
     m_pCurrentSprite = &m_aliveSprite;
     m_pCurrentFrames = &m_aliveFrames;
 
-    //Setup the Animation Timer
-    m_animationTimer.setInterval    (kTimerSpriteFrameChange);
-    m_animationTimer.setRepeatCount (CoreClock::Clock::kRepeatForever);
-    auto callback = COREGAME_CALLBACK_0(Bomb::onAnimationTimerTick, this);
-    m_animationTimer.setTickCallback(callback);
-
-    //
-    m_animationTimer.start();
     changeSpriteFrame(0);
+
     bool flipped = Lore::GameManager::instance()->getRandomNumber(0, 1);
     m_pCurrentSprite->setFlipX(flipped);
 }
 
+void Bomb::initTimers()
+{
+    //Setup the Animation Timer
+    m_animationTimer.setInterval   (kTimerSpriteFrameChange);
+    m_animationTimer.setRepeatCount(CoreClock::Clock::kRepeatForever);
+
+    auto animCallback = COREGAME_CALLBACK_0(Bomb::onAnimationTimerTick, this);
+    m_animationTimer.setTickCallback(animCallback);
+
+    m_animationTimer.start();
+
+    //Setup the Explode Timer.
+    m_explodeTimer.setInterval   (kTimerBombExplode);
+    m_explodeTimer.setRepeatCount(1);
+
+    auto explodeCallback = COREGAME_CALLBACK_0(Bomb::onExplodeTimerTick, this);
+    m_explodeTimer.setTickCallback(explodeCallback);
+}
+
+
+//Helpers
 void Bomb::setupFrames(const Lore::Sprite &sprite,
                        std::vector<Lore::Rectangle> &framesVec,
                        int framesCount)
@@ -93,7 +225,6 @@ void Bomb::setupFrames(const Lore::Sprite &sprite,
     }
 }
 
-
 void Bomb::changeSpriteFrame(int frameIndex)
 {
     m_frameIndex = frameIndex;
@@ -108,3 +239,8 @@ void Bomb::onAnimationTimerTick()
     changeSpriteFrame(frameIndex);
 }
 
+void Bomb::onExplodeTimerTick()
+{
+    m_state = State::Dead;
+    m_explodedFinishedCallback();
+}
