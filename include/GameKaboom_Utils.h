299731fd52f9acd59b071f50d05dad84cef02b0a
n2omatt <n2o.matt@gmail.com>
2016-05-31 00:45:49 -0300

    - Add a KABOOM_DLOG to better control the logs.

--- include/GameKaboom_Utils.h
+++ include/GameKaboom_Utils.h
@@ -6,4 +6,10 @@
 #define NS_GAMEKABOOM_END   }
 #define USING_NS_GAMEKABOOM using namespace GameKaboom;
 
+
+#include "CoreGame.h"
+
+#define KABOOM_DLOG(_fmt_, ...) \
+    COREGAME_DLOG(CoreGame::Log::Type::Debug1, (_fmt_), ##__VA_ARGS__)
+
 #endif // __Game_Kaboom_include_GameKaboom_Utils_h__ //
