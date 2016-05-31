876e4551dd728608d15153bf8380cf08d1dd1638
n2omatt <n2o.matt@gmail.com>
2016-05-31 00:45:36 -0300

    - Update the initialziation of SoundManager.

--- src/main.cpp
+++ src/main.cpp
@@ -25,7 +25,10 @@ int main()
 
     assetsMgr->initialize("./assets");
     inputMgr->initialize();
-    soundMgr->initialize();
+    soundMgr->initialize(MIX_DEFAULT_FREQUENCY,
+                         MIX_DEFAULT_FORMAT,
+                         MIX_DEFAULT_CHANNELS,
+                         2048);
 
     gameMgr->initialize(0, 60);
 
